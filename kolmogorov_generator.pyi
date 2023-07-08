4152a03538bd98c38e756a51a7432f77c582a49b
KanHar <knaan.harpaz@gmail.com>
2023-07-08 11:39:38 +0300

    Update docs

--- kolmogorov_generator.pyi
+++ kolmogorov_generator.pyi
@@ -12,3 +12,16 @@ class KolmogorovGen:
         seed: bytes,
     ): ...
     def generate(self) -> npt.NDArray[np.bool]: ...
+
+class KolmogorovInnerStateGen:
+    def __init__(
+        self,
+        inner_state_size: int,
+        outer_state_size: int,
+        states_discount_rate: float,
+        max_states: int,
+        max_steps: int,
+        filter_uniform_outputs: bool,
+        seed: bytes,
+    ): ...
+    def generate(self) -> npt.NDArray[np.bool]: ...
